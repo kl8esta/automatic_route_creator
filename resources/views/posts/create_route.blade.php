d6524806b15741e15b7a70286665a3d555090048
kl8esta <kraytesta@gmail.com>
2022-11-14 08:57:10 +0900

    Update a create_route page: Organize source codes

--- resources/views/posts/create_route.blade.php
+++ resources/views/posts/create_route.blade.php
@@ -11,45 +11,20 @@
         <h1>ルートガイド</h1>
         <input id="pac-input" class="controls" type="text" placeholder="Search Box"/>
         <div id="map" style="width: 700px; height: 500px;"></div>
-        <button id="btn" style="from-green-400 to-blue-500">CLICK</button>
+        <!--button id="btn" style="from-green-400 to-blue-500">CLICK</button><-->
         <h4>追加した観光地(回る順番は自動で決まります)</h4>
-        <!--table>
-            <tr id="disp_list" style="border-collapse: collapse; text-align: center;">
-                <td>スコア</td><td>スコア</td>
-            </tr>
-        </table>
-        <-->
         <h6>最大8か所追加できます(最初に出発地を入力してください)</h6>
         <ul id="disp_list">
         </ul>
-        <button id="delete_btn" style="from-green-400 to-blue-500">削除</button>
+        <!--button id="delete_btn" style="from-green-400 to-blue-500">削除</button><-->
         <form action="/post_route" method="POST">
             @csrf
-            <div class="search">
-                <h4>キーワード検索</h4>
-                <input type="text" name="post[title]" style="width:30%" placeholder="行きたい観光地名を入力して下さい" value="{{ old('post.title') }}"/>
-                <div>
-                <a href="/posts/myroute/private">検索</a>
-                <a href="/posts/myroute/private">観光地リストに追加</a>
-                </div>
-            </div>
-            <br>
             <div id="route-condition" class="form-group">
-                <div>
-                    <div style="float:right">
-                        <button id="route-button" class="btn btn-primary" type="button">検索</button>
-                        <button id="clear-button" class="btn btn-default" type="button">クリア</button>
-                    </div>
-                    <div style="clear:both"></div>
-                </div>                    
                 <div id="route-result" style="height: 52px;">
-                <div id="route-panel"></div>
+                    <div id="route-panel"></div>
                 </div>
             </div>
             <br>
-            <div class="spots_list">
-                <h4>観光地リスト</h4>
-            </div>
             <input type="submit" value="ルート投稿画面へ進む{最適なルートが計算されます}"/>
             <p><a href="/posts/post_route">APIができるまではこのページをクリック</a></p>
         </form>
@@ -90,8 +65,8 @@
             let markers = [];
             let add_btn;
             
-            // 観光地リストの表示場所
-            disp_list = document.getElementById('disp_list');
+            // 観光地{pタグ}リストの表示場所
+            const disp_list = document.getElementById('disp_list');
             
             //キーワードを変えたときの検索結果を変化させる処理
             searchBox.addListener("places_changed", () => {
@@ -112,7 +87,8 @@
                 const bounds = new google.maps.LatLngBounds();
                 // 場所ごとの情報をウィンドウ表示するための変数
                 const infoWindow = new google.maps.InfoWindow();
-                let lastOpenedInfoWindow = infoWindow;
+                let preInfoWindow = infoWindow;
+                
                 // 各場所に対する処理
                 places.forEach((place) => {
                     //console.log(place);
@@ -135,15 +111,6 @@
                         position: place.geometry.location,
                         optimized: false,
                     });
-              
-                    /*const infoWindow = new google.maps.InfoWindow({
-                        content: marker.getTitle() + "\r\n 【平均評価:" + String(marker.stars) + "/5.0】",
-                        ariaLabel: marker.getTitle(),
-                    });*/
-                    
-              
-                    //onclick="addPlace()"
-                    // 1つのマーカをクリックしたときに場所の情報をウィンドウ表示する処理
                 
                     // 各マーカを1つに格納
                     markers.push(marker);
@@ -157,28 +124,29 @@
                 
                 // マーカーウィンドウ内のボタン用
                 let add_btns = Array(markers.length);
+                // マーカーウィンドウの情報格納用
                 let place_info = Array(markers.length);
                 //let place_info = "";
-                function closeLastOpenedInfoWindow() {
-                    if (lastOpenedInfoWindow) {
-                        lastOpenedInfoWindow.close();
+                
+                // 前のマーカーで開いていたウィンドウの削除関数
+                function closeOtherMarkerWindow() {
+                    if (preInfoWindow) {
+                        preInfoWindow.close();
                     }
                 }
                 
-                let click_counts = 1;
-                let count_list =  Array(markers.length);
+                //domready時に前クリックしたマーカーウィンドウのボタン要素が残り続けることへの対策
+                //let click_counts = 1;
+                //let count_list =  Array(markers.length);
+                //let out_clicks = 0;
+                //let in_clicks = Array(markers.length);
                 
-                let out_clicks = 0;
-                let in_clicks = Array(markers.length);
-                
-                
+                //各マーカークリック時の処理
                 markers.forEach((marker, i) => {
-                    count_list[i] = 0;
-                    in_clicks[i] = 0; 
-                    //domready時に前クリックしたマーカーウィンドウのボタン要素が残り続けることへの対策
-                    //addable_flag = 0;
+                    //count_list[i] = 0;
+                    //in_clicks[i] = 0; 
                     marker.addListener("click", () => {
-                    //マーカをクリックしたときの場所情報のウィンドウ表示
+                        //マーカをクリックした際に表示するウィンドウのHTML記述
                         place_info[i] =
                             '<div id="window_contents" style="width: 200px; height: 200px;">' +
                             '<h3 id="firstHeading" class="firstHeading">'+
@@ -198,186 +166,116 @@
                             String(i) +
                             '" type="button">目的地に追加</button><p>'+
                             marker.address +
-                            "</p></div>";                    
-                            
-                        //infoWindow.close();
-                        console.log(marker);
-                        out_clicks += 1;
-                        in_clicks[i] = out_clicks;
+                            "</p></div>";  
+                        //console.log(marker);
+                        //out_clicks += 1;
+                        //in_clicks[i] = out_clicks;
+                        //console.log("外" + String(click_counts) + "+内" + String(i) + "番目" + String(count_list[i]));
                         
-                        console.log("外" + String(click_counts) + "+内" + String(i) + "番目" + String(count_list[i]));
-                        closeLastOpenedInfoWindow();
+                        // クリック直後に以前のマーカーウィンドウを閉じる
+                        //infoWindow.close();
+                        closeOtherMarkerWindow();
+                        // ウィンドウ用のHTMLをセットして開く
                         infoWindow.setContent(place_info[i]);
                         infoWindow.open(marker.getMap(), marker);
+                        
                         //if (in_clicks[i] == out_clicks)
                         //{
                         //infoWindow.addListener('domready', () => {
+                        // ウィンドウ内のタグ要素をアクセス可能にする
                         google.maps.event.addListenerOnce(infoWindow,'domready', function() {
-                            //console.log("infoWIndow is created");
-                            //const upper = document.getElementById('window_contents');
-                            console.log(add_btns[i]);
+                            //ウィンドウ内の観光地追加ボタンが押されたときの処理
                             add_btns[i] = document.getElementById('add_btn'+String(i));
-                            console.log(add_btns[i]);
                             //add_btn = document.getElementById('add_btn');
                             add_btns[i].addEventListener('click', () => {
                                 if(place_list.length < 8)
                                 {
-                                    count_list[i] = 1;
-                                    console.log(String(i) + "番目" + String(count_list[i]));
-                                    //addable_flag = 1;
-                                    //console.log(place);
-                                    //console.log("infoWIndow is created");
-                                    //place_list.push(marker.getTitle());
-                                    //if (addable_flag == 1)
-                                    console.log(place_list.length);
+                                    //count_list[i] = 1;
+                                    //console.log(String(i) + "番目" + String(count_list[i]));
                                     //if (count_list[i] == click_counts)
-                                    //{
+                                    
+                                    // 同じ観光地を追加しないための判定
                                     if (place_list.indexOf(marker.getTitle())==-1)
                                     {
+                                        // 観光地リストに追加
                                         place_list.push(marker.getTitle());                                
                                     }
-                                    console.log(place_list.length);
                                     
                                     // 一度pタグをすべて削除
                                     while(disp_list.firstChild){
                                         disp_list.removeChild(disp_list.firstChild);
                                     }
-                                    // pタグ内に追加した観光地を列挙
+                                    // pタグに観光地リストの要素を記述して列挙
                                     for (let i = 0; i < place_list.length; i++) 
-                                    { 
-                                        const todoList = document.createElement('p');
+                                    {
+                                        // pタグの生成
+                                        const pTag = document.createElement('p');
                                         // pタグの情報
                                         //todoList.id = 'place'+ String(disp_list.childElementCount);
-                                        todoList.id = 'place'+ String(i);
+                                        pTag.id = 'place'+ String(i);
                                         //todoList.textContent = marker.getTitle();
-                                        todoList.textContent = place_list[i];
+                                        pTag.textContent = place_list[i];
                                         // pタグ内に各観光地の削除ボタンを実装
                                         const btn = document.createElement('button')
-                                        //btn.id = 'btn' + String(i);
                                         btn.id = 'btn' + String(i);
                                         btn.type = "button";
                                         btn.textContent = "削除";
-                                        todoList.appendChild(btn);
-                                        disp_list.appendChild(todoList);
+                                        // 各pタグの子要素に追加
+                                        pTag.appendChild(btn);
+                                        // 各pタグを親要素に格納
+                                        disp_list.appendChild(pTag);
                                     }
-                　                   count_list[i] = 0;
+                　                   //count_list[i] = 0;
                 　                   }
-                　               else
-                　               {
-                　               window.confirm('目的地は(出発地を含めて)8個までです')
+                　               // 8個以上追加できないようにする
+                　               else {
+                　                   window.confirm('目的地は(出発地を含めて)8個までです')
                 　               }
-                　               
+                　               // 追加した観光地をリストから削除するボタンの有効化
                                 enableDelButtons();           　               
-                                //addable_flag = 0;
-                                //}
-                                //console.log(todoList);
-                                /*var disp_list_fc = disp_list.firstChild;
-                                for(var i=0; i<disp_list_fc; i++){
-                                    disp_list.removeChild(disp_list.getElementById('place'+ String(i)));
-                                }*/
-                                //disp_list.appendChild(todoList);
-                                /*infoWindow.close();
-                                infoWindow.setContent("added");
-                                infoWindow.open(marker.getMap(), marker);*/
-                                //console.log(disp_list);
-                                //infoWindow.close();
-                                /*while(disp_list.firstChild){
-                                    disp_list.removeChild(disp_list.firstChild);
-                                }*/
-                                /*for (let i = 0; i < place_list.length; i++) 
-                                { 
-                                　  const todoList = document.createElement('p');
-                                　  // pタグの情報
-                                　  todoList.id = 'place'+ String(i);
-    　                               todoList.textContent = place_list[i]; 
-                                　  // pタグ内に各観光地の削除ボタンを実装
-                                　  const btn = document.createElement('button')
-                                　  //btn.id = 'btn' + String(i);
-                                　  btn.id = 'btn';
-                                　  btn.type = "button";
-                                　  btn.textContent = "削除";
-                                　  todoList.appendChild(btn);
-    　                               disp_list.appendChild(todoList); 
-                                }*/
-                                /*var todoList = document.createElement('li'); 
-                　               todoList.textContent = place_list[place_list.length-1]; 
-                　               //todoList.textContent = marker.getTitle();
-            　                   disp_list.appendChild(todoList);*/
                             }) 
                             //place_list = [];
                         });
-                        //}
                         
-                        lastOpenedInfoWindow = infoWindow;
+                        preInfoWindow = infoWindow;
                         //count_list[i] = 0;                                    
-                        console.log("外" + String(click_counts) + "+内" + String(i) + "番目" + String(count_list[i]));
-                        /*infoWindow.addListener('closeclick', ()=>{
-                            for (let i = 0; i < place_list.length; i++) 
-                                { 
-                                    console.log(place_list[i]);
-                                }
-                        });*/
-    
-                        /*if ( place_list.length > num_places) 
-                        {
-                            //console.log(place_list.length);
-                            //let pre_status = place_list.length
-                        　  var todoList = document.createElement('li'); 
-                    　       console.log(places.length);
-            　               todoList.textContent = place_list[place_list.length-1]; 
-            　               document.getElementById('disp_list').appendChild(todoList);
-            　               num_places = place_list.length;
-            　               // console.log(place_list.length);
-                        }*/
-                        
-                        
+                        //console.log("外" + String(click_counts) + "+内" + String(i) + "番目" + String(count_list[i]));
                     });
                 });                    
                 
                 map.fitBounds(bounds);
             });
-            /*for (let i = 0; i < place_list.length; i++) 
-            { 
-            　  var todoList = document.createElement('li'); 
-            　  todoList.textContent = place_list[i]; 
-            　  document.getElementById('disp_list').appendChild(todoList); 
-            }*/
-            /*btn.addEventListener('click', function(){
-                //disp_list.removeChild(disp_list.firstChild);
-                for (let i = 0; i < place_list.length; i++) 
-                { 
-                    console.log(place_list[i]);
-                }
-            })*/
-            //console.log(place_ids[0]);
-            //const delete_btn = document.getElementById('delete_btn');            
+            
+            // 観光地をリストから削除する関数
             function enableDelButtons() {
-                // 各観光地のidと削除ボタンを取得
+                // 各観光地のpタグidと削除ボタンを取得
                 let place_ids = [];
                 let delete_btns = [];
+                
                 for(let i = 0; i < disp_list.childElementCount; i++)
                 {
-                    console.log(i);
+                    //pタグと削除ボタンタグを取得
                     place_ids.push(document.getElementById('place'+ String(i)));
-                    delete_btns.push(document.getElementById('btn'+ String(i)));
-                }
-                for(let i = 0; i < disp_list.childElementCount; i++)
-                {
-                    console.log(i);
-                    delete_btns[i].addEventListener('click', function(){
+                    del_listen = document.getElementById('btn'+ String(i));
+                    // リストから対象要素を削除詰めするクリックイベントの追加
+                    del_listen.addEventListener('click', function(){
                         disp_list.removeChild(place_ids[i]);
                         delete place_list[i];
                         place_list = place_list.filter(Boolean);
                     })
-                    
+                    delete_btns.push(del_listen);
                 }
+                //for(let i = 0; i < disp_list.childElementCount; i++)
+                //{
+                //    delete_btns[i].addEventListener('click', function(){
+                //        disp_list.removeChild(place_ids[i]);
+                //        delete place_list[i];
+                //        place_list = place_list.filter(Boolean);
+                //    })
+                //}
             }
         }
         window.initAutocmplete = initAutocomplete;
-        /*const btn = document.getElementById('btn');
-        btn.addEventListener('click', function(){
-            //disp_list.removeChild(disp_list.firstChild);
-        })*/
         
     </script>
     <script src="https://maps.googleapis.com/maps/api/js?key={{ $gapi }}&callback=initAutocomplete&libraries=places" defer>
